be46e31e0fece299c80f63e010d2b40fadd157fd
Emanuele Di Iorio <emanuelediioriodev@gmail.com>
2026-02-05 17:07:23 +0100

    Fix build errors: escape apostrophe and relax eslint rules

--- app/prenotazioni/[id]/page.js
+++ app/prenotazioni/[id]/page.js
@@ -196,7 +196,7 @@ export default function BookingDetailPage({ params }) {
                       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                       </svg>
-                      Note dall'Agenzia
+                      Note dall&apos;Agenzia
                     </h3>
                     <div className="prose prose-sm text-blue-900 whitespace-pre-line">
                       {booking.note_agenzia}
